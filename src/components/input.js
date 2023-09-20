a7e7262e2ea0bdc1efc412a1a21d1e9b0dd870cd
Artur Mostowski <artur.mostowski@protonmail.com>
2023-09-20 21:35:37 +0200

    mui without eui try

--- src/components/input.js
+++ src/components/input.js
@@ -1,70 +1,36 @@
 import React from 'react'
 import { registerAsWebComponent } from 'react-webcomponentify'
-import {
-  EuiButton,
-  EuiCheckboxGroup,
-  EuiFieldText,
-  EuiForm,
-  EuiFormRow,
-  EuiFilePicker,
-  EuiLink,
-  EuiRange,
-  EuiSelect,
-  EuiSpacer,
-  EuiSwitch,
-  EuiText,
-  useGeneratedHtmlId,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiButtonEmpty,
-} from '@elastic/eui'
-export const Input = () => {
-  return (
-    <div>
-      <p></p>
-      {['disabled'].map((value) => (
-        <>
-          <EuiFlexGroup key={value} responsive={false} gutterSize='s' alignItems='center'>
-            <EuiFlexItem grow={false}>
-              <EuiButtonEmpty
-                isDisabled={value === 'disabled' ? true : false}
-                color={value !== 'disabled' ? value : 'primary'}
-                onClick={() => {}}
-              >
-                {value.charAt(0).toUpperCase() + value.slice(1)}
-              </EuiButtonEmpty>
-            </EuiFlexItem>
 
-            <EuiFlexItem grow={false}>
-              <EuiButtonEmpty
-                size='s'
-                isDisabled={value === 'disabled' ? true : false}
-                color={value !== 'disabled' ? value : 'primary'}
-                onClick={() => {}}
-              >
-                Small2
-              </EuiButtonEmpty>
-            </EuiFlexItem>
-
-            <EuiFlexItem grow={false}>
-              <EuiButtonEmpty
-                size='xs'
-                isDisabled={value === 'disabled' ? true : false}
-                color={value !== 'disabled' ? value : 'primary'}
-                onClick={() => {}}
-              >
-                Extra small
-              </EuiButtonEmpty>
-            </EuiFlexItem>
-          </EuiFlexGroup>
-          <EuiSpacer size='s' />
-        </>
-      ))}
-    </div>
-  )
+export class Input extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { buttonText: '' }
+    this.onInputEnter = this.onInputEnter.bind(this)
+  }
+  onInputEnter(evt) {
+    this.setState({
+      buttonText: evt.target.value,
+    })
+  }
+  render() {
+    return (
+      <div>
+        <input
+          style='width:200px;height:40px;padding:10px;font-size:14px;border:2px solid blue;'
+          type='text'
+          placeholder='enter some text here'
+          onChange={this.onInputEnter}
+        />
+        <br />
+        <br />
+        <code>Value of this.state.buttonText: </code>
+        {this.state.buttonText}
+      </div>
+    )
+  }
 }
 
-registerAsWebComponent(Input, 'my-input', 'element')
+registerAsWebComponent(Input, 'my-input')
 
 /*
  In html: 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 // Include all the component here so that webpack can bundle them together
 import { MyButton } from './components/button'
-import { Input } from './components/input'
 import { MyComplexComponent } from './components/my-complex-component'
